6ebbdaad84d85829b96f22a97c8a055333394631
DustFalI <1540500607@qq.com>
2025-02-07 19:14:48 +0800

    Add files via upload

---
+++ Choise Number.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int n,k,arr[25],account=0;
+vector<int> num;
+bool judge(int sum)
+{
+	for(int i=sum-1;i>1;i--)
+	{
+		if(sum % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+void fuc(int start,int depth,int sum)
+{
+	if(depth == k)
+	{
+		if(judge(sum))
+		{
+			account++;
+		}
+		return;
+	}
+	for(int i=start;i<n;i++)
+	{
+		fuc(i + 1,depth + 1,sum + arr[i]);
+	}
+	num.pop_back();
+	return;
+}
+
+int main()
+{
+	cin>>n>>k;
+	for(int i = 0;i < n;i++)
+	{
+		cin>>arr[i];
+	}
+	fuc(0,0,0);//起始值和位数都从0开始 
+	cout<<account;
+	return 0;
+}
